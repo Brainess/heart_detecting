3ee3695e974a98c29ac5e2bfc2d86079edb077be
Brainess <fadeev.is20@physics.msu.ru>
2024-11-18 19:55:59 +0300

    Обрезка рабочей поверхности сердца; создание директорий; подсчёт ratio усреднённых каналов; парсинг по всем экспериментам в эпизоде

--- main.py
+++ main.py
@@ -82,76 +82,98 @@ def detection_contours(image_for_contours):
             cv2.putText(im_crop, "Prisoska", (x_cor, y_cor), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255))
 
 
-# переходим в рабочую директорию
-images_directory = f'R:/Experiments/Image'
-os.chdir(images_directory)
+# функция для расчёта усреднённого ratio-сигнала внутри присоски
+def ratio_of_mean_channels(images_directory):
+    # переходим в мышиную папку
+    os.chdir(images_directory)
 
-# создадим директории обработки
-os.makedirs('Blue', exist_ok=True)  # exist_ok=True предотвращает ошибку, если папка уже существует
-blue_dir = 'R:/Experiments/Image/Blue'
-os.makedirs('UV', exist_ok=True)
-uv_dir = 'R:/Experiments/Image/UV'
-os.makedirs('Mean_Ratio', exist_ok=True)
-ratio_dir = 'R:/Experiments/Image/Mean_Ratio'
-os.makedirs('Raw', exist_ok=True)
-raw_dir = 'R:/Experiments/Image/Raw'
+    # получаем список кадров, полученных от мыши
+    images = os.listdir()
 
-# перейдём в папку с кадрами
-os.chdir(raw_dir)
+    # создадим директории обработки
+    os.makedirs('Blue', exist_ok=True)  # exist_ok=True предотвращает ошибку, если папка уже существует
+    blue_dir = fr'{images_directory}\Blue'
+    os.makedirs('UV', exist_ok=True)
+    uv_dir = fr'{images_directory}\UV'
+    os.makedirs('Mean_Ratio', exist_ok=True)
+    ratio_dir = fr'{images_directory}\Mean_Ratio'
+    os.makedirs('Raw', exist_ok=True)
+    raw_dir = fr'{images_directory}\Raw'
 
-# получаем массив изображений в папке
-images = os.listdir()
+    for image in images:
+        shutil.move(image, raw_dir)
 
-# найдём первый кадр в УФ (frame_1_UV_check проверяет, что первый кадр в массиве является УФ)
-frame_UV, frame_BLue, frame_1_UV_check = UF_or_blue(cv2.imread(images[0], cv2.IMREAD_GRAYSCALE),
-                                                    cv2.imread(images[1], cv2.IMREAD_GRAYSCALE))
-
-
-# сделаем срезы кадров УФ и синего
-if frame_1_UV_check:
-    slice_UV = images[::2]
-    slice_blue = images[1::2]
-else:
-    slice_UV = images[1::2]
-    slice_blue = images[::2]
-
-
-# получим центр и радиус отверстия присоски
-center, radius = soska_detection(frame_UV)  # первая итерация
-# # cv2.circle(frame_UV, center, radius, (0, 0, 255), 3)
-# # viewImage(frame_UV, 'with circle')
-# n = 1.5   # множитель для радиуса
-# frame_UV = frame_UV[center1[0] - int(n * radius1):center1[0] + int(n * radius1),
-#                     center1[1] - int(n * radius1):center1[1] + int(n * radius1)]
-# center, radius = soska_detection(frame_UV)  # вторая итерация
-
-n = 0.5    # множитель радиуса
-
-for i in range(0, int(len(images)/2), 5):
-    images_uv = []
-    images_blue = []
-
-    # берём m-е усреднение
-    for m in range(i, i+5):
-        #   UV
-        shutil.copy(slice_UV[m], uv_dir)
-        images_uv.append(cv2.imread(slice_UV[m], cv2.IMREAD_GRAYSCALE)
-                         [center[0] - int(n * radius):center[0] + int(n * radius),
-                          center[1] - int(n * radius):center[1] + int(n * radius)])
-        #   Blue
-        shutil.copy(slice_blue[m], blue_dir)
-        images_blue.append(cv2.imread(slice_blue[m], cv2.IMREAD_GRAYSCALE)
-                           [center[0] - int(n * radius):center[0] + int(n * radius),
-                            center[1] - int(n * radius):center[1] + int(n * radius)])
-
-    mean_uv = np.mean(images_uv, axis=0).astype(np.float32)
-    mean_blue = np.mean(images_blue, axis=0).astype(np.float32)
-
-    frame_ratio = np.clip(mean_blue / mean_uv).astype(np.float32)
-    os.chdir(ratio_dir)
-    try:
-        check_write = cv2.imwrite(f'{i+5}.tif', frame_ratio)
-    except:
-        print('Проблемы с записью:')
-        raise
+    # перейдём в папку с кадрами
     os.chdir(raw_dir)
+
+    # найдём первый кадр в УФ (frame_1_UV_check проверяет, что первый кадр в массиве является УФ)
+    frame_UV, frame_BLue, frame_1_UV_check = UF_or_blue(cv2.imread(images[0], cv2.IMREAD_GRAYSCALE),
+                                                        cv2.imread(images[1], cv2.IMREAD_GRAYSCALE))
+
+    # сделаем срезы кадров УФ и синего
+    if frame_1_UV_check:
+        slice_UV = images[::2]
+        slice_blue = images[1::2]
+    else:
+        slice_UV = images[1::2]
+        slice_blue = images[::2]
+
+    # получим центр и радиус отверстия присоски
+    center, radius = soska_detection(frame_UV)  # первая итерация
+    # # cv2.circle(frame_UV, center, radius, (0, 0, 255), 3)
+    # # viewImage(frame_UV, 'with circle')
+    # n = 1.5   # множитель для радиуса
+    # frame_UV = frame_UV[center1[0] - int(n * radius1):center1[0] + int(n * radius1),
+    #                     center1[1] - int(n * radius1):center1[1] + int(n * radius1)]
+    # center, radius = soska_detection(frame_UV)  # вторая итерация
+
+    n = 0.5  # множитель радиуса
+
+    for i in range(0, int(len(images) / 2), 5):
+        images_uv = []
+        images_blue = []
+
+        # берём m-е усреднение
+        for m in range(i, i + 5):
+            #   UV
+            shutil.copy(slice_UV[m], uv_dir)
+            images_uv.append(cv2.imread(slice_UV[m], cv2.IMREAD_GRAYSCALE)
+                             [center[0] - int(n * radius):center[0] + int(n * radius),
+                             center[1] - int(n * radius):center[1] + int(n * radius)])
+            #   Blue
+            shutil.copy(slice_blue[m], blue_dir)
+            images_blue.append(cv2.imread(slice_blue[m], cv2.IMREAD_GRAYSCALE)
+                               [center[0] - int(n * radius):center[0] + int(n * radius),
+                               center[1] - int(n * radius):center[1] + int(n * radius)])
+
+        mean_uv = np.mean(images_uv, axis=0).astype(np.float32)
+        mean_blue = np.mean(images_blue, axis=0).astype(np.float32)
+
+        frame_ratio = np.clip(mean_blue / mean_uv).astype(np.float32)
+
+        os.chdir(ratio_dir)
+        try:
+            check_write = cv2.imwrite(f'{i + 5}.tif', frame_ratio)
+        except:
+            print('Проблемы с записью:')
+            raise
+        os.chdir(raw_dir)
+
+
+# определяем, где находится эпизод
+episode_directory = os.getcwd()
+
+# получаем список файлов в эпизоде
+mouses = os.listdir()
+
+mouse_directories = []
+
+# создаём список мышиных папок
+for mouse in mouses:
+    full_path = os.path.join(episode_directory, mouse)
+    if os.path.isdir(full_path):
+        mouse_directories.append(mouse)
+
+# парсим все изображения в папках
+for mouse_directory in mouse_directories:
+    ratio_of_mean_channels(fr'{episode_directory}\{mouse_directory}')
